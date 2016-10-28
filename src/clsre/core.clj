ebc36120d436778ab9958a7c4307acdfb010b103
Satosh Dhanyamraju <geeniuss01@gmail.com>
2016-10-28 18:39:26 +0530

    connecting namespaces to main

--- src/clsre/core.clj
+++ src/clsre/core.clj
@@ -1,7 +1,10 @@
 (ns clsre.core
+  (:use [codejam.counting-sheep :as sheep]
+          [fourclojure.fourclj :as c4])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+  (println (c4/to-roman (sheep/read-int)))
