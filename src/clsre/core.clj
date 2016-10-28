febd8a25f9113f84b7a15bbb8ce9ca02607c5a1c
Satosh Dhanyamraju <geeniuss01@gmail.com>
2016-10-28 19:12:03 +0530

    Some input validation

--- src/clsre/core.clj
+++ src/clsre/core.clj
@@ -1,10 +1,14 @@
 (ns clsre.core
   (:use [codejam.counting-sheep :as sheep]
-          [fourclojure.fourclj :as c4])
+        [fourclojure.fourclj :as c4])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-  (println (c4/to-roman (sheep/read-int)))
+  (cond
+    (not= 1 (count args)) (println "need exactly one number arg")
+    (not (number? (first args))) (println "enter only numbers")
+    (= (first args) 1) (println "Hello, World!")
+    :else (println "need one of these\n1  : hello world \n!1 : nothing"))
+  )
