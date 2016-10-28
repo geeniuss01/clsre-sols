0599fac7b5e0db322e243b45980c6db7be264ab8
Satosh Dhanyamraju <geeniuss01@gmail.com>
2016-10-28 18:16:51 +0530

    Added lein project template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clsre
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clsre/core_test.clj
@@ -0,0 +1,7 @@
+(ns clsre.core-test
+  (:require [clojure.test :refer :all]
+            [clsre.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
